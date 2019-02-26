e224b591854314b18357072642eb1e611b0f98f5
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-02-25 22:06:02 -0600

    (Add):Configuracion del servidor

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,25 @@
-const express =require('express');
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', 3000);
+
+// Midlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename(req, res, cb) {
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+});
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // Start server
 app.listen(app.get('port'), () =>(
